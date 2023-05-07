6be50a92cdf4648ae10007f6c5660098ef5e3fc1
oladejomubarak <oladejomubarakade@gmail.com>
2023-05-07 07:51:01 +0100

    setting up database

---
+++ src/test/java/com/mubarak/springsecurity/SpringsecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mubarak.springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
