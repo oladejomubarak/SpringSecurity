0ca8200cd5715681f4556061e4e575c25a5981a5
oladejomubarak <oladejomubarakade@gmail.com>
2023-05-09 15:03:13 +0100

    do filter chain

--- src/main/java/com/mubarak/springsecurity/config/JwtAuthenticationFilter.java
+++ src/main/java/com/mubarak/springsecurity/config/JwtAuthenticationFilter.java
@@ -13,12 +13,21 @@ import java.io.IOException;
 @Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final JwtService jwtService;
     @Override
     protected void doFilterInternal(
            @NonNull HttpServletRequest request,
            @NonNull HttpServletResponse response,
            @NonNull FilterChain filterChain
     ) throws ServletException, IOException {
-
+    final String authHeader = request.getHeader("Authorization");
+    final String jwt;
+    final String userEmail;
+    if(authHeader == null || !authHeader.startsWith("Bearer ")){
+        filterChain.doFilter(request, response);
+        return;
+    }
+    jwt = authHeader.substring(7);
+    userEmail = jwtService.extractUsername(jwt);
     }
 }
