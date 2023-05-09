9524f6343086b277ad8cffcfc75a6736a042d8c7
oladejomubarak <oladejomubarakade@gmail.com>
2023-05-09 17:49:47 +0100

    adding jwt dependencies

--- src/main/java/com/mubarak/springsecurity/config/JwtService.java
+++ src/main/java/com/mubarak/springsecurity/config/JwtService.java
@@ -1,5 +1,7 @@
 package com.mubarak.springsecurity.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -7,5 +9,12 @@ public class JwtService {
 
     public String extractUsername(String token) {
         return null;
+    }
+    private Claims extractAllClaims(String token){
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
     }
 }
