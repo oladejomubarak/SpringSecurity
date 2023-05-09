fec1fe8945f3a46620f26fb62a11e003317d8789
oladejomubarak <oladejomubarakade@gmail.com>
2023-05-09 18:02:12 +0100

    adding jwt dependencies

--- src/main/java/com/mubarak/springsecurity/config/JwtService.java
+++ src/main/java/com/mubarak/springsecurity/config/JwtService.java
@@ -2,19 +2,29 @@ package com.mubarak.springsecurity.config;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Base64;
 
 @Service
 public class JwtService {
-
+ private final String SECRET_KEY = "404E635266556A586E3272357538782F413F4428472B4B6250645367566B5970";
     public String extractUsername(String token) {
         return null;
     }
     private Claims extractAllClaims(String token){
         return Jwts.parserBuilder()
-                .setSigningKey(getSigningKey)
+                .setSigningKey(getSignInKey())
                 .build()
                 .parseClaimsJws(token)
                 .getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
