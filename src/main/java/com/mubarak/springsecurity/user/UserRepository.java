72ecfa36d44659248b2c9dee59596025e4ef0efa
oladejomubarak <oladejomubarakade@gmail.com>
2023-05-09 12:12:54 +0100

    creating models

---
+++ src/main/java/com/mubarak/springsecurity/user/UserRepository.java
@@ -0,0 +1,9 @@
+package com.mubarak.springsecurity.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findUserByEmail(String email);
+}
