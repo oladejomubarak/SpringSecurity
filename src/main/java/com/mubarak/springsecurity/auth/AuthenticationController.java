fcdca2c4912e518ad85c417358ea0791080aef8d
oladejomubarak <oladejomubarakade@gmail.com>
2023-05-13 04:22:24 +0100

    application configuration

--- src/main/java/com/mubarak/springsecurity/auth/AuthenticationController.java
+++ src/main/java/com/mubarak/springsecurity/auth/AuthenticationController.java
@@ -1,6 +1,7 @@
 package com.mubarak.springsecurity.auth;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,15 +12,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
 public class AuthenticationController {
+    @Autowired
+    private AuthenticationService service;
+
     @PostMapping("/register")
     public ResponseEntity<AuthenticationResponse> register(
             @RequestBody RegisterRequest request) {
-
+        return ResponseEntity.ok(service.register(request));
     }
 
     @PostMapping("/authenticate")
     public ResponseEntity<AuthenticationResponse> authenticate(
             @RequestBody AuthenticationRequest request) {
-
+        return ResponseEntity.ok(service.authenticate(request));
     }
 }
--- src/main/java/com/mubarak/springsecurity/auth/AuthenticationService.java
+++ src/main/java/com/mubarak/springsecurity/auth/AuthenticationService.java
@@ -1,10 +1,32 @@
 package com.mubarak.springsecurity.auth;
 
+import com.mubarak.springsecurity.user.Role;
+import com.mubarak.springsecurity.user.User;
+import com.mubarak.springsecurity.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AuthenticationService {
+    private final UserRepository repository;
 
+    private final PasswordEncoder passwordEncoder;
+
+    public AuthenticationResponse register(RegisterRequest request) {
+        var user = User.builder()
+                .firstname(request.getFirstname())
+                .lastname(request.getLastname())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+        repository.save(user);
+        return null;
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        return null;
+    }
 }
